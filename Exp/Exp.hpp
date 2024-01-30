ca2f1e5c68d1badaca1e6510d987781ed01251e3
gabik <gabidullinkamil1@gmail.com>
2024-01-30 23:08:01 +0300

    rebase from main

---
+++ Exp/Exp.hpp
@@ -0,0 +1,45 @@
+#include "Consts.hpp"
+#include <type_traits>
+#include <iostream>
+
+namespace ADAAI {
+    template<typename F>
+    F Exp(F x) { // return value is e^x
+        static_assert(std::is_floating_point<F>::value, "Not a floating point number\n");
+
+        F y = x / Ln2<F>; // e^x = 2^{x/ln2)
+
+        F y_int, y_float;
+        y_float = std::modf(y, &y_int);
+        if (y_int < INT32_MIN) {
+            return 0.0;
+        } else if (y_int > INT32_MAX) {
+            return INFINITY;
+        }
+        if(y_float > 0.5){  // |y_float| should be < 0.5
+            y_int ++;
+            y_float -= 1.0;
+        }
+        if(y_float < -0.5){  // |y_float| should be < 0.5
+            y_int --;
+            y_float += 1.0;
+        }
+
+        F x1 = y_float * Ln2<F>; // e^x = 2^[x] * e^{{x}*Ln2}. x1 = {x}*Ln2
+        F f1 = 1; // f1 = e^x1 - считаем Тейлором
+
+        F eps = Eps<F> * 10.0; // погрешность
+        // Start with n = 0
+        int n = 1;
+        F x_n = x1; // x^n
+        F fact_n = 1; // n!
+        while (std::abs(Sqrt2<F> * x_n / fact_n) >= eps) {
+            f1 += x_n / fact_n;
+            n++;
+            x_n *= x1;
+            fact_n *= n;
+        }
+        // хотим |y_float| < 0.5
+        return std::ldexp(f1, int(y_int)); // f1 * 2^n
+    }
+}
