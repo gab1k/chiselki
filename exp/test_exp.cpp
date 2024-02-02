9e322b7eedea1c9ab068fbc06f41709779d556da
Mitchell <mihaillitvinov26324@gmail.com>
2024-02-02 21:16:08 +0300

    add tests (first iteration)

--- exp/test_exp.cpp
+++ exp/test_exp.cpp
@@ -3,27 +3,52 @@
 
 #include "consts.hpp"
 #include "exp.hpp"
+#include <iostream>
 
 template<typename F>
-F absDifferent(F value) {
-    return std::abs(adaai::exp(value) - std::exp(value));
+void checkExp(F value, F eps, bool debug=false) {
+    const F current = adaai::exp<F>(value);
+    const F expected = std::exp(value);
+    CHECK((std::abs(current - expected) < eps ||
+            std::isinf(expected) && std::isinf(current) ||
+            std::isnan(expected) && std::isnan(current)));
+    if (debug) {
+        std::cout.precision(20);
+        std::cout << "Value: " << value << "\n" <<
+                    "Expected: " << expected << "\n" <<
+                    "Current: " << current << "\n";
+    }
 }
 
 template<typename F>
 void testWithTemplate() {
-    CHECK((absDifferent<F>(0.) < adaai::C_EPS<F>));
-    CHECK((absDifferent<F>(0.5) < adaai::C_EPS<F>));
-    CHECK((absDifferent<F>(1.5) < adaai::C_EPS<F>));
+    SUBCASE("Small") {
+        checkExp<F>(0., 0.00001);
+        checkExp<F>(0.0005, 0.00001);
+        checkExp<F>(0.123456789, 0.00001);
+        checkExp<F>(0.00000000005, 0.01);
+        checkExp<F>(0.00000000000000005, 0.01);
+    }
+    SUBCASE("Large") {
+        checkExp<F>(12., 0.01, true);
+    }
+    SUBCASE("Inf") {
+        checkExp<F>(105.5, 0.01);
+        checkExp<F>(112305.5, 0.01);
+    }
+    SUBCASE("Nan") {
+        checkExp<F>(NAN, 0.01);
+    }
 }
 
 TEST_CASE("Test exp function") {
-    SUBCASE("Test float") {
+    SUBCASE("Float") {
         testWithTemplate<float>();
     }
-    SUBCASE("Test double") {
+    SUBCASE("Double") {
         testWithTemplate<float>();
     }
-    SUBCASE("Test long double") {
+    SUBCASE("Long double") {
         testWithTemplate<float>();
     }
 }
