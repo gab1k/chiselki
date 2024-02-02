fb0e6e51082137f527c78ef31956b6e5b371f84a
Mitchell <mihaillitvinov26324@gmail.com>
2024-02-02 23:21:26 +0300

    update tests and some fix

--- exp/exp.hpp
+++ exp/exp.hpp
@@ -2,20 +2,24 @@
 #define EXP_HPP_
 
 #include "consts.hpp"
-#include <cstdint>
 #include <cmath>
+#include <cstdint>
 
 namespace adaai {
 
     template<typename F>
     constexpr F exp(F x) {  // return e^x
         static_assert(std::is_floating_point<F>::value, "Not a floating point number!");
+
+        /*
+            We can put long double instead of F in the intermediate
+            calculations to achieve maximum accuracy, but in this case
+            we may lose the speed of calculations
+        */
+
         if (std::isnan(x)) {
             return x;
         }
-        // We can put long double instead of F in the intermediate
-        // calculations to achieve maximum accuracy, but in this case
-        // we may lose the speed of calculations
 
         F y = x / C_LN_2<F>;  // e^x = 2^(x/ln2) = 2^y
         F y_int, y_float;
@@ -23,28 +27,24 @@ namespace adaai {
 
         if (y_int < INT32_MIN) {
             return 0.0;
-        } else if (y_int > INT32_MAX) {
+        }
+
+        if (y_int > INT32_MAX) {
             return INFINITY;
         }
 
-        if (y_float > 0.5) {  // |y_float| should be < 0.5
-            y_int++;
-            y_float -= 1.0;
-        }
-
-        if (y_float < -0.5) {  // |y_float| should be < 0.5
-            y_int--;
-            y_float += 1.0;
+        if (std::abs(y_float) > 0.5) {
+            y_int += (std::signbit(y_float) ? -1 : 1);
+            y_float += (std::signbit(y_float) ? 1 : -1);
         }
 
         F x1 = y_float * C_LN_2<F>; // e^x = 2^[x] * e^{{x}*Ln2}. x1 = {x}*Ln2
-        F eps = C_EPS<F>; // error rate
 
         F f1 = 0.0; // f1 = e^x1 - count by Taylor
         F next = 1.0;
         unsigned long long n = 0;
 
-        while (next * C_SQRT_2<F> >= eps) {
+        while (std::abs(next * C_SQRT_2<F>) >= C_EPS<F>) {
             f1 += next;
             next *= x1;
             next /= ++n;
--- exp/test_exp.cpp
+++ exp/test_exp.cpp
@@ -3,52 +3,92 @@
 
 #include "consts.hpp"
 #include "exp.hpp"
-#include <iostream>
+#include <cmath>
 
 template<typename F>
-void checkExp(F value, F eps, bool debug=false) {
+void checkExp(F value, F eps) {
     const F current = adaai::exp<F>(value);
     const F expected = std::exp(value);
-    CHECK((std::abs(current - expected) < eps ||
-            std::isinf(expected) && std::isinf(current) ||
-            std::isnan(expected) && std::isnan(current)));
-    if (debug) {
-        std::cout.precision(20);
-        std::cout << "Value: " << value << "\n" <<
-                    "Expected: " << expected << "\n" <<
-                    "Current: " << current << "\n";
-    }
+    CHECK((std::abs(current - expected) <= eps ||
+           std::isinf(expected) && std::isinf(current) ||
+           std::isnan(expected) && std::isnan(current)));
 }
 
 template<typename F>
-void testWithTemplate() {
-    SUBCASE("Small") {
-        checkExp<F>(0., 0.00001);
-        checkExp<F>(0.0005, 0.00001);
-        checkExp<F>(0.123456789, 0.00001);
-        checkExp<F>(0.00000000005, 0.01);
-        checkExp<F>(0.00000000000000005, 0.01);
+void testBasicWithTemplate() {
+    F eps = adaai::C_EPS<F>;
+
+    SUBCASE("Zero") {
+        checkExp<F>(0, eps);
     }
-    SUBCASE("Large") {
-        checkExp<F>(12., 0.01, true);
-    }
+
     SUBCASE("Inf") {
-        checkExp<F>(105.5, 0.01);
-        checkExp<F>(112305.5, 0.01);
+        checkExp<F>(12345.1234, eps);
+        checkExp<F>(INFINITY, eps);
+        checkExp<F>(-INFINITY, eps);
     }
-    SUBCASE("Nan") {
-        checkExp<F>(NAN, 0.01);
+
+    SUBCASE("NaN") {
+        checkExp<F>(NAN, eps);
+    }
+
+    SUBCASE("Small") {
+        checkExp<F>(0.1234321, eps);
+        checkExp<F>(0.0043212, eps);
+        checkExp<F>(0.0000392, eps);
+        checkExp<F>(0.0000087, eps);
+        checkExp<F>(0.0000007, eps);
+    }
+
+    SUBCASE("Large") {
+        eps = 1;
+        checkExp<F>(1.123423, eps);
+        checkExp<F>(2.432132, eps);
+        checkExp<F>(10.09876, eps);
+        checkExp<F>(13.33212, eps);
+        checkExp<F>(20.43221, eps);
     }
 }
 
-TEST_CASE("Test exp function") {
-    SUBCASE("Float") {
-        testWithTemplate<float>();
+
+TEST_CASE("Float") {
+    testBasicWithTemplate<float>();
+}
+
+TEST_CASE("Double") {
+    testBasicWithTemplate<double>();
+
+    SUBCASE("Small extra case") {
+        double eps = adaai::C_EPS<double>;
+        checkExp<double>(0.0000000987645657, eps);
+        checkExp<double>(0.0000000003213696, eps);
+        checkExp<double>(0.0000000000013326, eps);
+        checkExp<double>(0.0000000000000696, eps);
+        checkExp<double>(0.0000000000000006, eps);
     }
-    SUBCASE("Double") {
-        testWithTemplate<float>();
+
+    SUBCASE("Large extra case") {
+        double eps = 1.;
+        checkExp<double>(21.58393, eps);
+        checkExp<double>(28.43232, eps);
+        checkExp<double>(34.03276, eps);
     }
-    SUBCASE("Long double") {
-        testWithTemplate<float>();
+}
+
+TEST_CASE("Long double") {
+    testBasicWithTemplate<long double>();
+
+    SUBCASE("Small extra case") {
+        long double eps = adaai::C_EPS<long double>;
+        checkExp<long double>(0.0000000000987631579, eps);
+        checkExp<long double>(0.0000000000000000672, eps);
+        checkExp<long double>(0.0000000000000000004, eps);
+    }
+
+    SUBCASE("Large extra case") {
+        long double eps = 1.;
+        checkExp<long double>(35.9274, eps);
+        checkExp<long double>(39.423482, eps);
+        checkExp<long double>(42.032026, eps);
     }
 }
