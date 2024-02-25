e46ecc90099c07ce8803caf7ace8861bb92930b1
Mitchell <mihaillitvinov26324@gmail.com>
2024-02-25 21:33:04 +0300

    add chebyshev_coefficients pre-calculation

---
+++ useful/chebyshev_coefficients.cpp
@@ -0,0 +1,48 @@
+#include <gsl/gsl_linalg.h>
+
+void solve(int N) {
+    auto *a_data = (double *) calloc(N * N, sizeof(double));
+    auto *b_data = (double *) calloc(N, sizeof(double));
+    if (!a_data || !b_data) {
+        return;
+    }
+
+    for (int i = 0; i < N - 1; ++i) {
+        a_data[N * i + i] = -1;
+        for (int j = i + 1; j < N; ++j) {
+            if (j % 2 == 0 && i % 2 != 0 || j % 2 != 0 && i % 2 == 0) {
+                a_data[N * i + j] = 2 * j;
+            } else if (i % 2 != 0 && j == 0) {
+                a_data[N * i + j] = j;
+            }
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        double val = 0;
+        if (i % 4 == 0) val = 1;
+        if (i % 4 == 2) val = -1;
+        a_data[N * (N - 1) + i] = val;
+    }
+    b_data[N - 1] = 1;
+
+    gsl_matrix_view m = gsl_matrix_view_array(a_data, N, N);
+    gsl_vector_view b = gsl_vector_view_array(b_data, N);
+    gsl_vector *x = gsl_vector_alloc(N);
+
+    int s;
+    gsl_permutation *p = gsl_permutation_alloc(N);
+    gsl_linalg_LU_decomp(&m.matrix, p, &s);
+    gsl_linalg_LU_solve(&m.matrix, p, &b.vector, x);
+
+    gsl_vector_fprintf(stdout, x, "%.17g");
+
+    gsl_permutation_free(p);
+    gsl_vector_free(x);
+    free(b_data);
+    free(a_data);
+}
+
+int main() {
+    solve(15);
+    return 0;
+}
