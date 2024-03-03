7cafcd02760bf6e5a3bc2c73341c6ad7b9e2d110
gabik <gabidullinkamil1@gmail.com>
2024-03-03 22:35:03 +0300

    add coef

--- useful/chebyshev_polynomials.hpp
+++ useful/chebyshev_polynomials.hpp
@@ -7,13 +7,13 @@ namespace adaai {
     // {-1, 0, 3, 0, 0} = 3x^2 - 1
     class ChebyshevPolynomials {
     private:
-        std::vector<std::vector<int>> polynoms;
+        std::vector<std::vector<long long>> polynoms;
 
         void count_next() {
-            int n = polynoms.size();
-            std::vector<int> next(n + 2, 0);
+            unsigned long long n = polynoms.size();
+            std::vector<long long> next(n + 2, 0);
             for (int i = 0; i < n + 1; i++) {
-                next[i + 1] = 2 * polynoms[n - 1][i];
+                next[i + 1] = (polynoms[n - 1][i] << 1);
             }
             for (int i = 0; i < n; i++) {
                 next[i] -= polynoms[n - 2][i];
@@ -27,7 +27,7 @@ namespace adaai {
                         {0, 1}};
         }
 
-        explicit ChebyshevPolynomials(int k) {
+        explicit ChebyshevPolynomials(unsigned long long k) {
             polynoms = {{1},
                         {0, 1}};
             for (int i = 0; i < k - 1; i++) {
@@ -35,7 +35,7 @@ namespace adaai {
             }
         }
 
-        std::vector<int> get_polynomial(int k) {
+        std::vector<long long> get_polynomial(unsigned long long k) {
             while (polynoms.size() <= k) {
                 count_next();
             }
--- useful/fft_coefficients.cpp
+++ useful/fft_coefficients.cpp
@@ -9,22 +9,22 @@
 #include "chebyshev_polynomials.hpp"
 #include "../common/consts.hpp"
 
-double find_pol_in_x(const std::vector<int> &T, double x) {
-    double ans = 0;
-    double x_i = 1;
-    for (int coef: T) {
+long double find_pol_in_x(const std::vector<long long> &T, long double x) {
+    long double ans = 0;
+    long double x_i = 1;
+    for (long long coef: T) {
         ans += x_i * coef;
         x_i *= x;
     }
     return ans;
 }
 
-std::vector<double> solve(int N) {
-    std::vector<double> ans(N + 1); // k \in [0, N]
+std::vector<long double> solve(unsigned N) {
+    std::vector<long double> ans(N + 1); // k \in [0, N]
     adaai::ChebyshevPolynomials pols(N + 1);
-    for (int k = 0; k <= N; k++) {
-        for (int i = 1; i <= N + 1; i++) {
-            double x_i = cos((((double) (i * 2 - 1)) / ((double) (2 * N + 2)) * 3.14159265358979323846));
+    for (unsigned k = 0; k <= N; k++) {
+        for (unsigned i = 1; i <= N + 1; i++) {
+            long double x_i = cos((((double) (i * 2 - 1)) / ((double) (2 * N + 2)) * 3.14159265358979323846));
             ans[k] = std::exp(acos(x_i)) * find_pol_in_x(pols.get_polynomial(k), x_i);
             ans[k] = (ans[k] * 2) / (N + 1);
         }
@@ -33,9 +33,9 @@ std::vector<double> solve(int N) {
 }
 
 int main() {
-    std::vector<double> a = solve(20);
+    std::vector<long double> a = solve(20);
     std::cout.precision(17);
-    for(double a_k: a){
+    for(long double a_k: a){
         std::cout << a_k << ",\n";
     }
     return 0;
